15ba6fba64deb37cd1bc93d807e465d02a8d5693
Abhay Singhal <81774180+AbhaySinghal09@users.noreply.github.com>
2024-09-20 16:06:27 +0530

    Add files via upload

---
+++ emailing.py
@@ -0,0 +1,33 @@
+# try to use mobile hotspot , then the email function will work
+
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+
+PASSWORD="ejluyzsvaexipwue"
+SENDER="abhaysinghal09061999@gmail.com"
+RECEIVER="abhaysinghal09061999@gmail.com"
+
+def send_email(image_path):
+    print("send_email function started")
+    email_message=EmailMessage()
+    email_message["Subject"]="New customer showed up!"
+    email_message.set_content("Hey, we just saw a new customer!")
+
+    with open(image_path, "rb") as file:
+        content= file.read()
+    email_message.add_attachment(content,maintype="image",subtype=imghdr.what(None,content))
+
+    gmail=smtplib.SMTP("smtp.gmail.com",587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER,PASSWORD)
+    gmail.sendmail(SENDER,RECEIVER, email_message.as_string())
+    print("send_email function ended 1 ")
+    gmail.quit()
+    print("send_email function ended 2")
+
+if __name__=="__main__":
+    send_email(image_path="images/19.png")
+
